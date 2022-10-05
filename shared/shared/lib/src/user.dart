4114922474af039c6cb5b66f0b00d3dba97158fa
SuperMuel <samuel.mallet@supermuel.fr>
2022-10-05 14:52:38 +0200

    Imports fix

--- shared/shared/lib/src/user.dart
+++ shared/shared/lib/src/user.dart
@@ -2,8 +2,9 @@ import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:shared/src/i_entity.dart';
 
-import '../shared.dart';
 import 'id.dart';
+import 'username.dart';
+
 part 'user.freezed.dart';
 
 @freezed
