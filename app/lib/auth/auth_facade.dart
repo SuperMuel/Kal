a4d83f2924281d4c48cc3d0bbcf8d27c372783ec
SuperMuel <samuel.mallet@supermuel.fr>
2022-10-05 14:48:32 +0200

    First IAuthFacade

--- app/lib/auth/auth_facade.dart
+++ app/lib/auth/auth_facade.dart
@@ -3,3 +3,18 @@
 // Future<void> logInWithGoogle
 // Future<void> logInWithApple
 // Future <void> logOut
+
+import 'dart:async';
+
+import 'package:dartz/dartz.dart';
+import 'package:shared/shared.dart';
+
+abstract class IAuthFacade {
+  Stream<User> get userStream;
+
+  FutureOr<Option<User>> get currentUser;
+
+  Future<void> logInWithGoogle();
+
+  Future<void> logInWithApple();
+}
