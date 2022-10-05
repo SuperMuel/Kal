74b74e58ff7fa49767da8c656f213655e90c5259
SuperMuel <samuel.mallet@supermuel.fr>
2022-10-05 14:47:37 +0200

    Exported user and id models and IEntity interface

--- shared/shared/lib/shared.dart
+++ shared/shared/lib/shared.dart
@@ -7,3 +7,6 @@ export 'src/value_object.dart';
 export 'src/username.dart';
 export 'src/description.dart';
 export 'src/title50.dart';
+export 'src/user.dart';
+export 'src/id.dart';
+export 'src/i_entity.dart';
