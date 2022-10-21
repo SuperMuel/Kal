fbc5f616c8364d15f6c5c78322701102bea0b833
SuperMuel <samuel.mallet@supermuel.fr>
2022-10-21 18:20:13 +0200

    First package commit

---
+++ app/packages/kal_api_client/lib/kal_api_client.dart
@@ -0,0 +1,6 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library kal_api_client;
+
+export 'src/kal_api_client.dart' show KalApiClient;
---
+++ app/packages/kal_api_client/lib/src/kal_api_client.dart
@@ -0,0 +1,2 @@
+/// Base class to communicate with the official Kal API
+class KalApiClient {}
